a670872337fe4caca2bd49e6d92ef6ee3f8213d6
Sam Seay <samueljseay@gmail.com>
2018-06-02 14:26:45 +1200

    Authorize and load content from google photos.

--- js/components/app/component.js
+++ js/components/app/component.js
@@ -1,17 +1,106 @@
+import axios from "axios";
 import React, { Component } from "react";
-
 import Slideshow from "../slideshow";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { albums: [], media: [] };
+  }
+  componentDidMount() {
+    if (window.gapi) {
+      gapi.load("client:auth2", this.initializeClient.bind(this));
+    }
+  }
+
+  initializeClient() {
+    const scope = "https://www.googleapis.com/auth/photoslibrary.readonly";
+    gapi.client
+      .init({
+        scope,
+        clientId: ""
+      })
+      .then(() => {
+        this.GoogleAuth = gapi.auth2.getAuthInstance();
+        const user = this.GoogleAuth.currentUser.get();
+
+        if (user.hasGrantedScopes(scope)) {
+          this.fetchAlbums();
+        } else {
+          this.GoogleAuth.signIn();
+        }
+
+        this.GoogleAuth.isSignedIn.listen(() => {
+          this.fetchAlbums();
+        });
+      });
+  }
+
+  async fetchAlbums() {
+    const scope = "https://www.googleapis.com/auth/photoslibrary.readonly";
+    const user = this.GoogleAuth.currentUser.get();
+
+    if (user.hasGrantedScopes(scope)) {
+      const authToken = gapi.client.getToken().access_token;
+
+      var config = {
+        headers: { Authorization: `Bearer ${authToken}` }
+      };
+
+      const { data } = await axios.get(
+        "https://photoslibrary.googleapis.com/v1/albums",
+        config
+      );
+
+      this.setState({ albums: data.albums });
+    }
+  }
+
+  chooseAlbum(albumId) {
+    this.fetchAlbumContents(albumId);
+  }
+
+  async fetchAlbumContents(albumId) {
+    const authToken = gapi.client.getToken().access_token;
+
+    var config = {
+      headers: { Authorization: `Bearer ${authToken}` }
+    };
+
+    const { data } = await axios.post(
+      "https://photoslibrary.googleapis.com/v1/mediaItems:search",
+      { albumId },
+      config
+    );
+
+    this.setState({ media: data.mediaItems });
+  }
+
   render() {
+    const photos = this.state.media;
+
+    if (photos.length) {
+      return <Slideshow imageUrls={photos.map(photo => photo.baseUrl)} />;
+    }
+
     return (
-      <Slideshow
-        imageUrls={[
-          "https://picsum.photos/300/400",
-          "https://picsum.photos/500/400",
-          "https://picsum.photos/500/200"
-        ]}
-      />
+      <div>
+        <h3>Choose an Album</h3>
+        <ul>
+          {this.state.albums.map((album, i) => (
+            <li key={i}>
+              <a
+                onClick={() => {
+                  this.chooseAlbum(album.id);
+                }}
+                href="#"
+              >
+                {album.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
